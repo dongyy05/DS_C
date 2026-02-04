a1413e062ec53a5a3d019949aa75e35f6cf13126
dongyy <2736728711@qq.com>
2026-02-04 17:25:07 +0800

    Declare API for doubly linked list operations in header

--- DList/include/Dlist.h
+++ DList/include/Dlist.h
@@ -19,4 +19,19 @@ typedef struct DList{
     int size;
 }List;
 
+void InitDList(List* mylist);
+Node* _buynode(ElementType x);
+void push_back(List* mylist, ElementType x);
+void push_front(List* list, ElementType x);
+void show_list(List* list);
+void pop_back(List* list);
+void pop_front(List* list);
+void insert_val(List* list, ElementType x);
+PNode find_val(List* list, ElementType key);
+int length(List* list);
+void delete_val(List* list, ElementType key);
+void sort(List* list);
+void reverse(List* list);
+void clear(List* list);
+void destroy(List* list);
 #endif
